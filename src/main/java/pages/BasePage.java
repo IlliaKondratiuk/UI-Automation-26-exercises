8fc3b9f51a6786d2574461675547bcad3fe77942
Illia <qa.kond.work@gmail.com>
2025-02-19 14:11:57 +0100

    Add: scrollToElement method in BasePage.java

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,10 +2,8 @@
 
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -42,5 +40,12 @@ public class BasePage {
 
     public void fillInput(By el, String text) {
         driver.findElement(el).sendKeys(text);
+    }
+
+    public void scrollToElement(By locator) {
+        WebElement el = driver.findElement(locator);
+        new Actions(driver)
+                .scrollToElement(el)
+                .perform();
     }
 }
