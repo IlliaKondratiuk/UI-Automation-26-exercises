8207cd5d6400e9362d34afd19531d56fabc6ce09
Illia <qa.kond.work@gmail.com>
2025-02-20 22:52:11 +0100

    Add: Exercise 9 test class and logic, SearchResults.txt for the exercise, locators and methods for the exercise in ProductsPage.java

--- src/main/java/pages/ProductsPage.java
+++ src/main/java/pages/ProductsPage.java
@@ -7,12 +7,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.List;
 
 public class ProductsPage extends BasePage {
 
     By productList = new By.ByClassName("single-products");
+
+    By searchInput = new By.ByName("search");
+    By searchButton = new By.ById("submit_search");
+    By searchedProducts = new By.ByXPath("//h2[text()=\"Searched Products\"]");
+    By searchResults = new By.ByXPath("//div[@class='single-products']//div//p");
+
     String viewProductXPathBegin = "//a[@href='/product_details/";
 
     public ProductsPage(WebDriver driver) {
@@ -37,4 +46,32 @@ public class ProductsPage extends BasePage {
         clickElement(new By.ByXPath(viewProductXPathBegin + productIndex + "']"));
     }
 
+    public void enterProduct(String product) {
+        fillInput(searchInput, product);
+    }
+
+    public void clickSearchButton() {
+        clickElement(searchButton);
+    }
+
+    public boolean isSearchedProductsVisible() {
+        return isElementVisible(searchedProducts);
+    }
+
+    public boolean areSearchResultsCorrect(String nameFilePath) throws IOException {
+        //Extracting the expected names of the results from the .txt file
+        List<String> expectedNames = Files.readAllLines(Paths.get(nameFilePath));
+
+        //Finding all the collapsed TC names on the page
+        List<WebElement> productResults = driver.findElements(searchResults);
+
+        //Comparing the name of each element in the list with each expected name
+        for (int i = 0; i < expectedNames.size(); i++) {
+            if (!productResults.get(2 * i).getText().equals(expectedNames.get(i))) {
+                String temp = productResults.get(i).getText();
+                return false;
+            }
+        }
+        return true;
+    }
 }
---
+++ src/test/java/Exercise9SearchTest.java
@@ -0,0 +1,60 @@
+import config.Base;
+import io.qameta.allure.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import pages.MainPage;
+import pages.ProductsPage;
+
+import java.io.IOException;
+
+public class Exercise9SearchTest extends Base {
+
+    private static MainPage mainPage;
+    private static ProductsPage productsPage;
+
+    //Path to the file with the expected product search results
+    private final String filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\SearchResults.txt";
+
+    @Before
+    public void begin() {
+        driver.get("https://automationexercise.com/");
+
+        mainPage = new MainPage(driver);
+        mainPage.handleCookies();
+    }
+
+    @Test
+    @Epic("Products")
+    @Feature("Products page")
+    @Story("User can search for a product and see all relevant results")
+    @Severity(SeverityLevel.CRITICAL)
+    public void Ex9SearchProduct() {
+        //Step 1. Check if the logo is visible.
+        Assert.assertTrue("Step 1: The logo is not visible", mainPage.logoIsVisible());
+
+        //Step 2. Click on 'Products' button
+        mainPage.clickProductsButton();
+
+        //Step 3. Verify user is navigated to ALL PRODUCTS page successfully
+        Assert.assertEquals("Step 3: The user is not navigated to the 'All Products' page.",
+                "https://automationexercise.com/products", driver.getCurrentUrl());
+
+        //Step 4. Enter product name in search input and click search button
+        productsPage = new ProductsPage(driver);
+        productsPage.enterProduct("Dress");
+        productsPage.clickSearchButton();
+
+        //Step 5. Verify 'SEARCHED PRODUCTS' is visible
+        Assert.assertTrue("Step 5: 'SEARCHED PRODUCTS' is not visible",
+                productsPage.isSearchedProductsVisible());
+
+        //Step 6. Verify all the products related to search are visible
+        try {
+            Assert.assertTrue("Step 6: The search results are incorrect or not visible.",
+                    productsPage.areSearchResultsCorrect(filePath));
+        } catch (IOException e) {
+            Assert.fail("Step 6: An error occurred trying to read the file with expected expected search results");
+        }
+    }
+}
