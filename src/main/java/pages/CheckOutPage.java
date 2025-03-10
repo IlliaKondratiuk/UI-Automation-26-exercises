3137ebdf6c47ac4c4edccaa9c27d8f4756cf7b83
Illia <qa.kond.work@gmail.com>
2025-03-10 16:53:18 +0100

    Add: CheckOutPage.java page class

---
+++ src/main/java/pages/CheckOutPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckOutPage extends BasePage {
+
+    By checkOutButton = new By.ByXPath("//a[contains(@class, 'check_out')]");
+
+    public CheckOutPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickCheckOutButton() {
+        clickElement(checkOutButton);
+    }
+}
