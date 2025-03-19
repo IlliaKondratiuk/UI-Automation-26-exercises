920d30d717455560b9aa444288de164414986cbe
Illia <qa.kond.work@gmail.com>
2025-03-19 16:11:55 +0100

    Add: categoryList and categoryFromDetailsList to TestContext.java as they are required for exercise 18

--- src/main/java/config/TestContext.java
+++ src/main/java/config/TestContext.java
@@ -4,11 +4,31 @@ package config;
 
 import org.openqa.selenium.WebDriver;
 
+import java.util.ArrayList;
+
 public class TestContext {
     private WebDriver driver;
+    private ArrayList<String> categoryList;
+    private ArrayList<String> categoryFromDetailsList;
 
     public WebDriver getDriver() {
         return driver;
+    }
+
+    public ArrayList<String> getCategoryList() {
+        return categoryList;
+    }
+
+    public ArrayList<String> getCategoryFromDetailsList() {
+        return categoryFromDetailsList;
+    }
+
+    public void setCategoryFromDetailsList(ArrayList<String> categoryFromDetailsList) {
+        this.categoryFromDetailsList = categoryFromDetailsList;
+    }
+
+    public void setCategoryList(ArrayList<String> categoryList) {
+        this.categoryList = categoryList;
     }
 
     public void setDriver(WebDriver driver) {
