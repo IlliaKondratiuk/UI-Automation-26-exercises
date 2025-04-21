e2d40059b67a38da266a93dc8bb11a1da41b88c8
Illia <qa.kond.work@gmail.com>
2025-04-21 15:12:50 +0200

    Add: FileUtils.java

---
+++ src/main/java/utils/FileUtils.java
@@ -0,0 +1,22 @@
+//This class contains methods for managing file checks.
+
+package utils;
+
+import java.io.File;
+
+public class FileUtils {
+
+    public static boolean isFileDownloaded(String downloadDir, String fileName) {
+        File dir = new File(downloadDir);
+        File[] dirContents = dir.listFiles();
+
+        if (dirContents != null) {
+            for (File file : dirContents) {
+                if (file.getName().equals(fileName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
