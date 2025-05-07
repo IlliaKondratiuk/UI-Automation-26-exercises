f357f029628160976bed5723c67d309c6bc9a841
Illia <qa.kond.work@gmail.com>
2025-05-07 13:38:55 +0200

    Fix: migrate Exercise5RegisterExistingEmailTest to JUnit 5

--- src/test/java/Exercise5RegisterExistingEmailTest.java
+++ src/test/java/Exercise5RegisterExistingEmailTest.java
@@ -1,29 +1,29 @@
 // This is "Test Case 5: Register User with existing email" from the Automation Exercise page at automationexercise.com
 // Steps 1(Launch browser) and 2(Navigate to url) have been skipped as common sense
-// Using JUnit 4.13.2, designing with Page Object Model and generating an Allure(2.32.2) report
+// Using JUnit 5.10.0, designing with Page Object Model and generating an Allure(2.32.2) report
 // The user data is parsed from src\resources\invalidregister.properties
 
 import config.PageNames;
 import config.testConfigs.BaseTest;
 
 import io.qameta.allure.*;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
 import pages.*;
 
 import java.util.ResourceBundle;
 
 @Tag("Critical")
-public class Exercise5RegisterExistingEmailTest extends BaseTest {
+class Exercise5RegisterExistingEmailTest extends BaseTest {
 
     private static MainPage mainPage;
     private static LoginPage loginPage;
 
     private static ResourceBundle values;
 
-    @Before
+    @BeforeEach
     public void begin() {
         driver.get(PageNames.MAIN.getUrl());
 
@@ -38,16 +38,17 @@ public class Exercise5RegisterExistingEmailTest extends BaseTest {
     @Feature("Login functionality")
     @Story("As a user, I cannot register with an email that is already in use so that my account remains unique and secure.")
     @Severity(SeverityLevel.CRITICAL)
-    public void Ex5RegisterExistingEmailTest() {
+    void Ex5RegisterExistingEmailTest() {
         //Step 1. Check if the logo is visible.
-        Assert.assertTrue("Step 1: The logo is not visible", mainPage.logoIsVisible());
+        Assertions.assertTrue(mainPage.logoIsVisible(), "Step 1: The logo is not visible");
 
         //Step 2. Click on 'Signup / Login' button
         mainPage.clickSignupLogin();
 
         //Step 3. Verify 'New User Signup!' is visible
         loginPage = new LoginPage(driver);
-        Assert.assertTrue("Step 3: The sign up text is not visible",loginPage.isSignUpTextVisible());
+        Assertions.assertTrue(loginPage.isSignUpTextVisible(),
+                "Step 3: The sign up text is not visible");
 
         //Step 4. Enter name and email address
         loginPage.enterNameEmailSignup(values.getString("username"), values.getString("email"));
@@ -56,8 +57,8 @@ public class Exercise5RegisterExistingEmailTest extends BaseTest {
         loginPage.clickSignUpButton();
 
         //Step 6. Verify error 'Email Address already exist!' is visible
-        Assert.assertTrue("Step 6: 'Email Address already exist!' is not visible",
-                loginPage.isAlreadyExistsVisible());
+        Assertions.assertTrue(loginPage.isAlreadyExistsVisible(),
+                "Step 6: 'Email Address already exist!' is not visible");
     }
 
 }
