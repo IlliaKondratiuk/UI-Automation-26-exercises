fce7a220e0e07cfbb97fa59291eb88d19c36a689
Illia <qa.kond.work@gmail.com>
2025-05-07 13:38:39 +0200

    Fix: migrate Exercises4LogoutTest to JUnit 5

--- src/test/java/Exercise4LogoutTest.java
+++ src/test/java/Exercise4LogoutTest.java
@@ -1,16 +1,16 @@
 // This is "Test Case 4: Logout User" from the Automation Exercise page at automationexercise.com
 // Steps 1(Launch browser) and 2(Navigate to url) have been skipped as common sense
-// Using JUnit 4.13.2, designing with Page Object Model and generating an Allure(2.32.2) report
+// Using JUnit 5.10.0, designing with Page Object Model and generating an Allure(2.32.2) report
 // The user data is parsed from src\resources\validlogin.properties
 
 import config.PageNames;
 import config.testConfigs.BaseTest;
 
 import io.qameta.allure.*;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import pages.LoginPage;
 import pages.MainPage;
 
@@ -24,7 +24,7 @@ public class Exercise4LogoutTest extends BaseTest {
 
     private static ResourceBundle values;
 
-    @Before
+    @BeforeEach
     public void begin() {
         driver.get(PageNames.MAIN.getUrl());
 
@@ -41,15 +41,16 @@ public class Exercise4LogoutTest extends BaseTest {
     @Severity(SeverityLevel.CRITICAL)
     public void Ex4LogoutTest() {
         //Step 1. Check if the logo is visible.
-        Assert.assertTrue("Step 1: The logo is not visible", mainPage.logoIsVisible());
+        Assertions.assertTrue(mainPage.logoIsVisible(),
+                "Step 1: The logo is not visible");
 
         //Step 2. Click on 'Signup / Login' button
         mainPage.clickSignupLogin();
 
         //Step 3. Verify 'Login to your account' is visible
         loginPage = new LoginPage(driver);
-        Assert.assertTrue("Step 3: The 'Login to your account' label is not visible",
-        loginPage.isLoginTextVisible());
+        Assertions.assertTrue(loginPage.isLoginTextVisible(),
+                "Step 3: The 'Login to your account' label is not visible");
 
         //Step 4. Enter valid/invalid email address and password
         loginPage.enterEmailPassLogin(values.getString("email"), values.getString("pass"));
@@ -59,14 +60,14 @@ public class Exercise4LogoutTest extends BaseTest {
 
         //Step 6. Verify that 'Logged in as username' is visible
         mainPage.initUsername(values.getString("username"));
-        Assert.assertTrue("Step 6: 'Logged in as username' is not visible or the username is incorrect",
-                mainPage.checkLoggedInAsLabelNav());
+        Assertions.assertTrue(mainPage.checkLoggedInAsLabelNav(),
+                "Step 6: 'Logged in as username' is not visible or the username is incorrect");
 
         //Step 7. Click 'Logout' button
         mainPage.clickLogOut();
 
         //Step 8. Verify that user is navigated to login page
-        Assert.assertEquals("Step 8: The user is not navigated to the login page after logging out.",
-                "https://automationexercise.com/login", driver.getCurrentUrl());
+        Assertions.assertEquals("https://automationexercise.com/login", driver.getCurrentUrl(),
+                "Step 8: The user is not navigated to the login page after logging out.");
     }
 }
