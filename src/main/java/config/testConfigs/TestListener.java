a794dbb245d17cd2fe2408010db1fa7290fde3a1
Illia <qa.kond.work@gmail.com>
2025-05-09 15:22:06 +0200

    Add: placeholder methods in TestListener.java

--- src/main/java/config/testConfigs/TestListener.java
+++ src/main/java/config/testConfigs/TestListener.java
@@ -1,4 +1,41 @@
 package config.testConfigs;
 
-public class TestListener {
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+
+import java.util.Optional;
+
+public class TestListener implements TestWatcher, BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+    @Override
+    public void testDisabled(ExtensionContext context, Optional<String> reason) {
+        TestWatcher.super.testDisabled(context, reason);
+    }
+
+    @Override
+    public void testSuccessful(ExtensionContext context) {
+        TestWatcher.super.testSuccessful(context);
+    }
+
+    @Override
+    public void testAborted(ExtensionContext context, Throwable cause) {
+        TestWatcher.super.testAborted(context, cause);
+    }
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        TestWatcher.super.testFailed(context, cause);
+    }
+
+    @Override
+    public void beforeTestExecution(ExtensionContext extensionContext) throws Exception {
+
+    }
+
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
+
+    }
 }
