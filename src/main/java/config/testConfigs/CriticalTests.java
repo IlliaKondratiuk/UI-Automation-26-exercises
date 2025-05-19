56f8f7313f4476e855153673dc95b642aba9b5f9
Illia <qa.kond.work@gmail.com>
2025-05-19 15:42:24 +0200

    Remove: unused CriticalTests.java

--- src/main/java/config/testConfigs/CriticalTests.java
+++
@@ -1,7 +0,0 @@
-//This class is used with the @Category annotation in test classes for exercises 1 to 12.
-//This enables separate test execution using "mvn test -Dgroups=CriticalTests"
-//Yet, all tests from 1 to 12 are critical, so all of them have this category
-package config.testConfigs;
-
-public interface CriticalTests {
-}
