5215a2ed8177b04aa8ed15b41fb49114910677ad
Illia <qa.kond.work@gmail.com>
2025-05-28 14:55:45 +0200

    Fix: change screenshot on fail to use Allure instead of utils

--- src/main/java/config/testConfigs/TestLogger.java
+++ src/main/java/config/testConfigs/TestLogger.java
@@ -1,9 +1,13 @@
 package config.testConfigs;
 
+import config.WebDriverConfig;
+import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +30,11 @@ public class TestLogger implements TestWatcher, BeforeTestExecutionCallback, Aft
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
         logger.error("❌ Test failed: {}. Reason: {}", context.getDisplayName(), cause.toString());
+        try {
+            takeScreenshot();
+        } catch (Exception e) {
+            logger.error("⚠️ Could not take screenshot: {}", e.getMessage());
+        }
     }
 
     @Override
@@ -41,5 +50,10 @@ public class TestLogger implements TestWatcher, BeforeTestExecutionCallback, Aft
     @Override
     public void afterTestExecution(ExtensionContext context) throws Exception {
         logger.info("✅ Test ended: {}", context.getDisplayName());
+    }
+
+    @Attachment(value = "Screenshot on failure", type = "image/png")
+    private byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverConfig.getDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
